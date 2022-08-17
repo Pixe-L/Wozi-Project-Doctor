27a18d97dfb97c8936aa38ce6f0ab226c2cd88da
pixel <tecnoaddictt@gmail.com>
2022-08-17 12:28:57 -0500

    doceavo commit, finalizando todo el responsive del proyecto

--- JS/main.js
+++ JS/main.js
@@ -93,24 +93,4 @@ $('.product-carousel').slick({
  slidesToScroll: 1,
  adaptiveHeight: true,
  arrows: true,
- responsive: [
-  {
-   breakpoint: 768,
-   settings: {
-    arrows: false,
-    centerMode: true,
-    centerPadding: '40px',
-    slidesToShow: 2,
-   },
-  },
-  {
-   breakpoint: 480,
-   settings: {
-    arrows: false,
-    centerMode: true,
-    centerPadding: '40px',
-    slidesToShow: 1,
-   },
-  },
- ],
 });
